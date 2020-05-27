b66be2d5c525ce86c96950f20510979008c04e6b
layuri.silva <yuri.silva@thoughtworks.com>
2020-05-27 14:58:24 -0300

    Updates

---
+++ precache-manifest.030bb37e147a581862a1eafd03e0d0c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f32048fe695cfcc74aadbde9562ad6e",
+    "url": "/consciencia-negra/index.html"
+  },
+  {
+    "revision": "066322c2010f409e5a85",
+    "url": "/consciencia-negra/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "138e7fd1f60977e8aa36",
+    "url": "/consciencia-negra/static/js/2.af0d43c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/consciencia-negra/static/js/2.af0d43c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066322c2010f409e5a85",
+    "url": "/consciencia-negra/static/js/main.a246648e.chunk.js"
+  },
+  {
+    "revision": "ed2f052ac91460217385",
+    "url": "/consciencia-negra/static/js/runtime-main.ffd8dd60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/consciencia-negra/static/media/logo.5d5d9eef.svg"
+  }
+]);
