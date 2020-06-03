f748258daf7f94319decc6a55f61e4bf314f38d5
layuri.silva <yuri.silva@thoughtworks.com>
2020-06-03 17:23:30 -0300

    Implementação para o teste da homepage - estrutura inicial - Ramon e Yuri

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './pages/Home/Home.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Awesome project coming soon.
-        </p>
-      </header>
-    </div>
+    <Home />
   );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.PureComponent{
+    render() {
+        return(
+            <div>
+                <p>O ConscienciaNegra.com.br é um portal dedicado à conscientização, reflexão e compartilhamento de conhecimento e cultura negra no Brasil.</p>
+                <p>"Não basta não ser racista, é preciso ser antirracista."- Angela Davis</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
