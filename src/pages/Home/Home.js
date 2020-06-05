e10a356d6d986da232ecfb54ba24d66c8d84fbe9
Ramon Vicente <ramonvicentee@gmail.com>
2020-06-05 18:15:07 -0300

    Inclusão de react bootstrap e alteração na Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import '../../bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Home extends React.PureComponent{
     render() {
         return(
-            <div>
-                <p>O ConscienciaNegra.com.br é um portal dedicado à conscientização, reflexão e compartilhamento de conhecimento e cultura negra no Brasil.</p>
-                <p>"Não basta não ser racista, é preciso ser antirracista."- Angela Davis</p>
-            </div>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col md="8">
+                        <p>O ConscienciaNegra.com.br é um portal dedicado à conscientização, reflexão e compartilhamento de conhecimento e cultura negra no Brasil.</p>
+                        <p>"Não basta não ser racista, é preciso ser antirracista."- Angela Davis</p>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
