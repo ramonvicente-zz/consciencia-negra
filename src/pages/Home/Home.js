c2b2d83edcade2e2df6c7880bebfaaf71857c1d7
layuri.silva <yuri.silva@thoughtworks.com>
2020-06-08 17:15:01 -0300

    Criação das divs da homepage e posicionamento dos elementos na página - Ramon e Yuri

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../bootstrap.min.css';
+import './Home.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,14 +8,19 @@ import Col from 'react-bootstrap/Col';
 class Home extends React.PureComponent{
     render() {
         return(
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Col md="8">
-                        <p>O ConscienciaNegra.com.br é um portal dedicado à conscientização, reflexão e compartilhamento de conhecimento e cultura negra no Brasil.</p>
-                        <p>"Não basta não ser racista, é preciso ser antirracista."- Angela Davis</p>
-                    </Col>
-                </Row>
-            </Container>
+                <Container fluid className="banner">
+                    <Row className="justify-content-md-center">
+                        <div class="logo">
+                        </div>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md="8" className="welcome">
+                            <h1>Bem vinda!</h1>
+                            <p>O ConscienciaNegra.com.br é um portal dedicado à conscientização, reflexão e compartilhamento de conhecimento e cultura negra no Brasil.</p>
+                            <p>"Não basta não ser racista, é preciso ser antirracista."- Angela Davis</p>
+                        </Col>
+                    </Row>
+                </Container>
         );
     }
 }
